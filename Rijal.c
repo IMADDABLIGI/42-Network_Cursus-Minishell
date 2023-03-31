da83d622d4a79f613cdfd860271c83a9ef2ec5e4
hznagui <houdayfahouayfa7@gmail.com>
2023-03-31 13:25:54 +0000

    done deal

--- Rijal.c
+++ Rijal.c
@@ -1,23 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   Rijal.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/31 13:24:32 by hznagui           #+#    #+#             */
-/*   Updated: 2023/03/31 13:24:32 by hznagui          ###   ########.fr       */
+/*   Created: 2023/03/31 13:24:27 by hznagui           #+#    #+#             */
+/*   Updated: 2023/03/31 13:24:27 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <readline/readline.h>
-#include <readline/history.h>
 
-int main()
-{
-    printf( "%s\n", readline( " " ) );
+int main() {
+    char *input;
+    
+    input = readline("minishell :");
+    
+    printf("You entered: %s\n", input);
+    
+    free(input);
     return 0;
 }

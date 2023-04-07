6067b80b7829c45175b34a3335bef119df556936
Imad Dabligi <imad.dabligi@gmail.com>
2023-04-07 13:34:27 +0000

    CHECK

--- Pipex/CHECK/ft_check_cmd.c
+++ Pipex/CHECK/ft_check_cmd.c
@@ -6,7 +6,7 @@
 /*   By: idabligi <idabligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 01:17:54 by idabligi          #+#    #+#             */
-/*   Updated: 2023/04/07 01:37:28 by idabligi         ###   ########.fr       */
+/*   Updated: 2023/04/07 13:32:36 by idabligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int ft_check_path(char *path)
 {
-    if (access(path, F_OK) == 0)// Check if a path to a file is exist or no. 
-        return true;
-    return false;
+	if (access(path, F_OK) == 0)// Check if a path to a file is exist or no. 
+		return true;
+	return false;
 }
 
 int	ft_check_cmd(char *cmd)
@@ -24,14 +24,16 @@ int	ft_check_cmd(char *cmd)
 	int		i;
 	int		check;
 	char	*path;
+	char	**s_cmd;
 	char	**s_path;// splitting the variable $PATH with (:) from env command.
 
 	i = 0;
-    path = getenv("PATH");
+	path = getenv("PATH");
+	s_cmd = ft_split(cmd, ' ');
 	s_path = ft_split(path, ':');// you're free to use ur own ft_split no problem.
 	while (s_path[i])
 	{
-		if (access(ft_strjoin(s_path[i], cmd), X_OK) == 0)//strjoin has been modified, check it.
+		if (access(ft_strjoin(s_path[i], s_cmd[0]), X_OK) == 0)//strjoin has been modified, check it.
 			return true;//CMD has been found.
 		i++;
 	}
--- Pipex/CHECK/main.c
+++ Pipex/CHECK/main.c
@@ -6,7 +6,7 @@
 /*   By: idabligi <idabligi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 01:44:41 by idabligi          #+#    #+#             */
-/*   Updated: 2023/04/07 01:57:23 by idabligi         ###   ########.fr       */
+/*   Updated: 2023/04/07 13:32:11 by idabligi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int main()
 {
-    if (ft_check_cmd("wc -l"))
+    if (ft_check_cmd("ls -l"))
         printf("HAD LCOMMAND KAYNA\n");//You can try every CMD you like.
     else
         printf("LA WALLO HAD LCOMMAND GA3MA KAYNA\n");//if a valid CMD wouldn't have been found, that means u have to make it ur own and not by execve standard function.
 
     //-------------------------------------------------//
     
-    if (ft_check_path("/Users/idabligi/Desktop/Minishell"))//Read the path again.
+    if (ft_check_path("/Users/hznagui/Desktop/Minishell"))//Read the path again.
         printf("HAD PATH KAYN\n");
     else
         printf("ah youssef zaml\n");

96e8a673e13b15fdfa5479a6da4b4bb6fc8cae21
hznagui <houdayfahouayfa7@gmail.com>
2023-04-11 01:55:47 +0000

    done deal

--- Pipex/CHECK/ft_check_cmd.c
+++ Pipex/CHECK/ft_check_cmd.c
@@ -6,7 +6,7 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 01:17:54 by idabligi          #+#    #+#             */
-/*   Updated: 2023/04/10 17:15:45 by hznagui          ###   ########.fr       */
+/*   Updated: 2023/04/11 01:55:02 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,20 @@ int ft_check_path(char *path)
 		return true;
 	return false;
 }
-
+size_t legnth(char **k)
+{
+	size_t i;
+	
+	i=0;
+	while (k[i++]);
+	return(i);
+}
 int	ft_check_cmd(char *cmd)
 {
 	int		i;
 	int		check;
 	char	*path;
+	char *join;
 	char	**s_path;// splitting the variable $PATH with (:) from env command.
 
 	i = 0;
@@ -31,9 +39,11 @@ int	ft_check_cmd(char *cmd)
 	s_path = ft_split(path, ':');// you're free to use ur own ft_split no problem.
 	while (s_path[i])
 	{
-		if (access(ft_strjoin(s_path[i], cmd), X_OK) == 0)//strjoin has been modified, check it.
-			return true;//CMD has been found.
+		join=ft_strjoin(s_path[i], cmd);
+		if (access(join, X_OK) == 0)//strjoin has been modified, check it.
+			return (free_all(s_path,legnth(s_path)),true);
+		free(join);//CMD has been found.
 		i++;
 	}
-	return false;//CMD not found.
+	return (free_all(s_path,legnth(s_path)),false);//CMD not found.
 }
--- Pipex/CHECK/main.c
+++ Pipex/CHECK/main.c
@@ -3,15 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: idabligi <idabligi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/06 01:44:41 by idabligi          #+#    #+#             */
-/*   Updated: 2023/04/07 13:32:11 by idabligi         ###   ########.fr       */
+/*   Updated: 2023/04/11 01:43:43 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void test()
+{
+    system("leaks a.out");
+}
 int main()
 {
     if (ft_check_cmd("ls -l"))
@@ -25,4 +29,5 @@ int main()
         printf("HAD PATH KAYN\n");
     else
         printf("ah youssef zaml\n");
+        atexit(test);
 }
